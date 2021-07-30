1c2f60df4110daab8c0d5adb4029cd8dac73481c
sher-zulunov <zulunovsherzod@gmail.com>
2021-07-30 18:38:33 +0500

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,37 @@
       <div class="currenc-container">
         <currenc-button
           :currenc="this.btc"
-          :balance="this.balance"
+          :balance="this.balancebtc"
+          @input-balance="toggleInputForm"
+          @withdraw-balance="toggleWithdrawForm"
         ></currenc-button>
         <currenc-button
           :currenc="this.doge"
-          :balance="this.balance"
+          :balance="this.balancedoge"
         ></currenc-button>
-        <currenc-button :currenc="this.ltc"></currenc-button>
-        <input-form />
-        <currenc-button :currenc="this.shib"></currenc-button>
-        <currenc-button :currenc="this.bnb"></currenc-button>
-        <currenc-button :currenc="this.usd"></currenc-button>
-        <currenc-button :currenc="this.rur"></currenc-button>
+        <currenc-button
+          :currenc="this.ltc"
+          :balance="this.balanceltc"
+        ></currenc-button>
+        <div v-show="isInput" class="currenc-send">
+          <input-form @add-balance="addBalance"></input-form>
+        </div>
+        <currenc-button
+          :currenc="this.shib"
+          :balance="this.balanceshib"
+        ></currenc-button>
+        <currenc-button
+          :currenc="this.bnb"
+          :balance="this.balancebnb"
+        ></currenc-button>
+        <currenc-button
+          :currenc="this.usd"
+          :balance="this.balanceusd"
+        ></currenc-button>
+        <currenc-button
+          :currenc="this.rur"
+          :balance="this.balancerur"
+        ></currenc-button>
       </div>
     </div>
   </div>
@@ -41,8 +60,27 @@ export default {
       bnb: "BNB",
       usd: "USD",
       rur: "RUR",
-      balance: 0.0,
+      balancebtc: 0,
+      balancedoge: 0,
+      balanceltc: 0,
+      balanceshib: 0,
+      balancebnb: 0,
+      balanceusd: 0,
+      balancerur: 0,
+      isInput: false,
     };
+  },
+  methods: {
+    toggleInputForm() {
+      this.isInput = !this.isInput;
+    },
+    toggleWithdrawForm() {
+      this.isInput = !this.isInput;
+    },
+    addBalance(balance) {
+      this.balancebtc += Number(balance);
+      console.log("2");
+    },
   },
 };
 </script>
--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,35 +1,42 @@
 <template>
-  <transition name="fade">
-    <div v-if="show" class="currenc-send">
-      <input
-        type="number"
-        class="currenc-send-input"
-        placeholder="сумма"
-        name="balance"
-      />
-      <input class="currenc-send-input" type="text" placeholder="адрес" />
-      <p class="tax">Коммисия 5%</p>
-      <textarea
-        name=""
-        id=""
-        cols="70"
-        rows="2"
-        placeholder="Комментарий"
-      ></textarea>
-      <div class="input-value-cont">
-        <input
-          id="balance"
-          @click="updateBalance"
-          class="input-value"
-          type="submit"
-          value="Ввести"
-        />
-      </div>
+  <form @submit.prevent="addBalance">
+    <input
+      type="number"
+      class="currenc-send-input"
+      placeholder="сумма"
+      name="balance"
+      v-model.lazy.trim="balance"
+    />
+    <input class="currenc-send-input" type="text" placeholder="адрес" />
+    <p class="tax">Коммисия 5%</p>
+    <textarea
+      name=""
+      id=""
+      cols="70"
+      rows="2"
+      placeholder="Комментарий"
+    ></textarea>
+    <div class="input-value-cont">
+      <input id="balance" class="input-value" type="submit" value="Ввести" />
     </div>
-  </transition>
+  </form>
 </template>
 <script>
 export default {
   name: "InputForm",
+  methods: {
+    addBalance() {
+      if (this.balance === 0) {
+        return;
+      }
+      this.$emit("add-balance", this.balance);
+      this.balance = null;
+    },
+  },
+  data() {
+    return {
+      balance: null,
+    };
+  },
 };
 </script>
